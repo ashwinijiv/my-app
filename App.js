6ebbb1905aed494cb4456c5eb7bf9931cf53a302
ashwinijiv <90909865+ashwinijiv@users.noreply.github.com>
2023-11-27 00:24:57 +0530

    React router and hosting on Github

--- App.js
+++ App.js
@@ -1,72 +1,61 @@
 // import logo from './logo.svg';
 import { useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
     })
     setTimeout(() => {
       setAlert(null);
-    },2000);
+    }, 2000);
   }
 
-  const removeBodyClasses = ()=>{
-    document.body.classList.remove('bg-light')
-    document.body.classList.remove('bg-dark')
-    document.body.classList.remove('bg-warning')
-    document.body.classList.remove('bg-danger')
-    document.body.classList.remove('bg-success')
-  }
-
-  const toggleMode = (cls)=>{
-    removeBodyClasses();
-    document.body.classList.add('bg-'+cls)
-    if (cls === 'light'){
-      setMode('light')
-    }
-    if (cls === 'dark'){
-      setMode('dark')
-    }
+  const toggleMode = (cls) => {
     //  To toggle switch between light and dark only 
-    // if (mode === 'light'){
-    //   setMode('dark');
-    //   document.body.style.backgroundColor = '#334e76'
-    //   showAlert("Dark mode has been enabled", "success");
-    // }
-    // else{
-    //   setMode('light');
-    //   document.body.style.backgroundColor = 'white'
-    //   showAlert("Light mode has been enabled", "success");
-    // }
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#334e76'
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode"
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode"
+    }
   }
-    
+
 
   return (
     <>
-    {/* <Navbar title="TextUtils" about="About us" mode={mode} toggleMode={toggleMode}/> */}
-    <Navbar title="TextUtils" about="About us" toggleMode={toggleMode}/>
-    {/* <Navbar/> */}
-    <Alert alert={alert}/>
-    <div className='container'>
-    <TextForm heading="Enter text here to analyze" mode={mode} showAlert={showAlert}/>
-    {/* <About/> */}
-    </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} about="About us" toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className='container my-3'>
+          <Routes>
+            <Route exact path="/about" element={<About/>} />
+            <Route exact path="/" element={<TextForm heading="Enter text here to analyze" mode={mode} showAlert={showAlert} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
 
 export default App;
-
-
-
-
